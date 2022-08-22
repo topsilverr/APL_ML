a60afd28517f44c77c44b13e2ac09e08b42d18db
지상은 <wltkddms9909@naver.com>
2022-08-22 22:58:21 +0900

    importig

--- ARIMA.py
+++ ARIMA.py
@@ -1,1 +1,44 @@
-APL 연구에 필요한 ARIMA 모듈 Test
+# 기본
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# 경고 뜨지 않게 설정
+import warnings
+warnings.filterwarnings('ignore')
+
+# 그래프 설정
+plt.rcParams['font.family'] = 'Malgun Gothic'
+# plt.rcParams['font.family'] = 'AppleGothic'
+plt.rcParams['font.size'] = 16
+plt.rcParams['figure.figsize'] = 20, 10
+plt.rcParams['axes.unicode_minus'] = False
+
+# ARIMA (시계열 예측)
+from statsmodels.tsa.arima_model import ARIMA
+import statsmodels.api as sm
+
+# 시간 측정을 위한 시간 모듈
+import datetime
+
+
+
+# 데이터 수집
+import requests
+from bs4 import BeautifulSoup
+import re
+import time
+import os
+import json
+
+# 한국어 형태소 분석
+from konlpy.tag import Okt, Hannanum, Kkma, Mecab, Komoran
+
+# 워드 클라우드를 위한 라이브러리
+from collections import Counter
+import pytagcloud
+from IPython.display import Image
+
+# 저장
+import pickle
